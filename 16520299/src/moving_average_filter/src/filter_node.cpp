87793ca640bc4378ee00e9c23b868008907c82be
malikrafsan <pro.malikakbar2357@gmail.com>
2021-06-24 23:02:57 +0700

    implement param server & queue for filtering data

--- 16520299/src/moving_average_filter/src/filter_node.cpp
+++ 16520299/src/moving_average_filter/src/filter_node.cpp
@@ -1,14 +1,21 @@
 #include "ros/ros.h"
 #include <std_msgs/Float64.h>
-#include <typeinfo>
+#include <exception>
+//#include <vector>
+#include <queue>
+#include <sstream>
+
 
 class FilterNode{
   private:
     int N;
     int count = 0;
     double datum;
-    double arr[5];
     double sumArr = 0;
+    bool filtering = false;
+
+    std::queue<double> arr;
+    
     std_msgs::Float64 sensor_measurement_filtered;
 
     ros::Publisher pub;
@@ -18,65 +25,66 @@ class FilterNode{
     FilterNode(ros::NodeHandle *n){
       sub = n->subscribe("sensor_measurement", 25, &FilterNode::chatterCallback, this);
       pub = n->advertise<std_msgs::Float64>("sensor_measurement_filtered", 25);
-      ros::param::get("/size_array", N);
+      if (ros::param::has("/size_N")) {
+        ros::param::get("/size_N", N);
+      } else {
+        ROS_INFO("PARAMETER '/size_N' HAS TO BE DEFINE FIRST");
+        throw std::exception();
+      }
       ROS_INFO("PARAM: [%d]", N);
-      isiArr();
+      //isiArr();
     }
 
+    /*
     void isiArr() {
       for (int i=0;i<N;i++){
         arr[i] = 0;
       }
     }
+    */
 
     double filterData(double rawData) {
-      sumArr = sumArr - arr[count % N] + rawData;
-      arr[count % N] = rawData;
+      sumArr = sumArr - arr.front() + rawData;
+      //arr[count % N] = rawData;
+      arr.pop(); 
+      arr.push(rawData);
       return (sumArr / N);
     }
 
     void chatterCallback(const std_msgs::Float64::ConstPtr& msg) {
-      ROS_INFO("I heard: [%f]", msg->data);
-      datum = filterData(msg->data);
-      ROS_INFO("added: [%f]" ,datum);
-      ROS_INFO("count: %d", count);
+      ++count;
+      ROS_INFO("I heard: [%lf]", msg->data);
+      if (filtering) {
+        datum = filterData(msg->data);
+        ROS_INFO("Filtered: [%g]" ,datum);
+      } else {
+        sumArr += msg->data;
+        arr.push(msg->data);
+        if (count == N) {
+          ROS_INFO("Filtered: [%g]", (sumArr / N));
+          filtering = true;
+        } else {
+          ROS_INFO("Still waiting enough data");
+        }
+      }
+      ROS_INFO("Count: %d\n", count);
+      showq(arr);
       
       sensor_measurement_filtered.data = datum;
       pub.publish(sensor_measurement_filtered);
-      
-      ++count;
+    }
+
+    void showq(std::queue<double> gq) {
+        std::queue<double> g = gq;
+        while (!g.empty()) {
+            //cout << '\t' << g.front();
+            ROS_INFO("%g \t",g.front());
+            g.pop();
+        }
+        ROS_INFO("\n");
     }
 
 };
-
-//const int N = 5;
-//int count = 0;
-//double datum;
-//double arr[N];
-//double sumArr = 0;
-//std_msgs::Float64 sensor_measurement_filtered;
-
-//void isiArr() {
-//  for (int i=0;i<N;i++){
-//    arr[i] = 0;
-//  }
-//}
-
-/*
-double filterData(double rawData) {
-  sumArr = sumArr - arr[count % N] + rawData;
-  arr[count % N] = rawData;
-  return (sumArr / N);
-}
-
-void chatterCallback(const std_msgs::Float64::ConstPtr& msg) {
-  ROS_INFO("I heard: [%f]", msg->data);
-  datum = filterData(msg->data);
-  ROS_INFO("added: [%f]" ,datum);
-  ROS_INFO("count: %d", count);
-  ++count;
-}
-*/
 
 int main(int argc, char **argv) {
   ros::init(argc, argv, "filter_node");
@@ -85,27 +93,11 @@ int main(int argc, char **argv) {
 
   FilterNode node = FilterNode(&n);
 
-  /*
-  ros::Subscriber sub = n.subscribe("sensor_measurement", 25, chatterCallback);
-  ros::Publisher pub = n.advertise<std_msgs::Float64>("sensor_measurement_filtered", 25);
-
-	ros::Rate loop_rate(0.2);
-
-  isiArr();
-
-  */
-
   ros::Rate loop_rate(0.2);
   while (ros::ok()){
-
-    //sensor_measurement_filtered.data = datum;
-
-    //pub.publish(sensor_measurement_filtered);
-
 		ros::spinOnce();
 		loop_rate.sleep();
   }
-
 
   return 0;
 }
