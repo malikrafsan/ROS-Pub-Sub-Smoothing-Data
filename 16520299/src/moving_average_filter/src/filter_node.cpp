db3d0cae029ceda261085d09408a9b27dca3c26c
malikrafsan <pro.malikakbar2357@gmail.com>
2021-06-24 11:57:47 +0700

    update program, filter_node publish filtered data

--- 16520299/src/moving_average_filter/src/filter_node.cpp
+++ 16520299/src/moving_average_filter/src/filter_node.cpp
@@ -2,11 +2,15 @@
 #include <std_msgs/Float64.h>
 #include <typeinfo>
 
+int count = 0;
+double datum;
+std_msgs::Float64 sensor_measurement_filtered;
 
 void chatterCallback(const std_msgs::Float64::ConstPtr& msg) {
   ROS_INFO("I heard: [%f]", msg->data);
-  double datum = msg->data + 2;
+  datum = (msg->data * 10) * (msg->data * 10);
   ROS_INFO("added: [%f]" ,datum);
+  ROS_INFO("count: %d", count);
 }
 
 int main(int argc, char **argv) {
@@ -15,8 +19,27 @@ int main(int argc, char **argv) {
   ros::NodeHandle n;
 
   ros::Subscriber sub = n.subscribe("sensor_measurement", 25, chatterCallback);
+  ros::Publisher pub = n.advertise<std_msgs::Float64>("sensor_measurement_filtered", 25);
   
-  ros::spin();
+  //std::cout << "Main program" << datum << std::endl;
+
+  //ros::spin();
+
+	ros::Rate loop_rate(100);
+
+	ros::spinOnce();
+
+  while (ros::ok()){
+
+    sensor_measurement_filtered.data = datum;
+
+    pub.publish(sensor_measurement_filtered);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+		++count;
+  }
+
 
   return 0;
 }
